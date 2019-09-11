bf62e22759643e3f748b33ad9b144ee84f76f2c9
HuyHoangPhi <huyphi116@gmail.com>
2019-09-11 20:10:01 +1000

    Show mapkit, current location

--- UberClone/MapViewController.cs
+++ UberClone/MapViewController.cs
@@ -1,13 +1,55 @@
 using Foundation;
 using System;
 using UIKit;
-
+using MapKit;
+using CoreLocation;
+using CoreGraphics; 
 namespace UberClone
 {
     public partial class MapViewController : UIViewController
     {
+        private MapViewDelegate mapDelegate;
+        private MKMapView mapView; 
         public MapViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+
+            // set the map view 
+            mapView = new MKMapView(UIScreen.MainScreen.Bounds);
+            mapView.MapType = MKMapType.Standard; 
+            View = mapView;
+            mapView.ShowsUserLocation = true;
+            mapView.AddAnnotation(new MKPointAnnotation()
+            {
+                Title = "Your Location",
+                Coordinate = new CLLocationCoordinate2D(42.364260, -71.120824)
+            });
+
+            // set the map delegate
+            mapDelegate = new MapViewDelegate();
+            mapView.Delegate = mapDelegate;
+
+
+            // Add map center 
+            const double lat = 42.374260;
+            const double lon = -71.120824;
+            var mapCenter = new CLLocationCoordinate2D(lat, lon);
+            var mapRegion = MKCoordinateRegion.FromDistance(mapCenter, 2000, 2000);
+            mapView.CenterCoordinate = mapCenter;
+            mapView.Region = mapRegion;
+
+
+
+
+            // display user's current location 
+            CLLocationManager locationManager = new CLLocationManager();
+            locationManager.RequestWhenInUseAuthorization();
+
         }
     }
 }
---
+++ UberClone/MapViewDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MapKit;
+using UIKit; 
+namespace UberClone
+{
+    public class MapViewDelegate : MKMapViewDelegate
+    {
+        string pId = "PinAnnotation";
+        public override MKAnnotationView GetViewForAnnotation(MKMapView mapView, IMKAnnotation annotation)
+        {
+            MKAnnotationView pinView = null;
+
+            if (annotation is MKUserLocation)
+                return pinView;
+
+            pinView = mapView.DequeueReusableAnnotation(pId);
+
+            if (pinView == null)
+                pinView = new MKPinAnnotationView(annotation, pId);
+
+            ((MKPinAnnotationView)pinView).PinColor = MKPinAnnotationColor.Red;
+            pinView.CanShowCallout = true;
+            pinView.RightCalloutAccessoryView = UIButton.FromType(UIButtonType.DetailDisclosure);
+            pinView.Draggable = true; 
+            return pinView;
+        }
+    }
+}
